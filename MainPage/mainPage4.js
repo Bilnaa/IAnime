2a457ceeecbdcc365b87e412a6a6e790494e678b
Stanislas Pierre bombolong <danyspb4ever@gmail.com>
2022-06-03 03:55:03 +0000

    main page 5

--- MainPage/mainPage4.js
+++ MainPage/mainPage4.js
@@ -214,7 +214,21 @@
             donnes.push(new Data(image, '',type, title, 'Top 30', '', '', false, link));
         }
     }
-    output.push(new Output(CellDesings.wide11, Orientation.horizontal, DefaultLayouts.wideFull, Paging.leading, new Section('Le top 30 Animes :', true), null, donnes));
+
+                let ok = [];
+                let dat = {};
+                            
+                for (let i in donnes) {
+                    compare = donnes[i].title;
+                    compa  = donnes[i].type;
+                    dat[compare] = donnes[i];
+                    dat[compa] = donnes[i];
+                }          
+                for (i in dat) {
+                    ok.push(dat[i]);
+                }
+
+    output.push(new Output(CellDesings.wide11, Orientation.horizontal, DefaultLayouts.wideFull, Paging.leading, new Section('Le top 30 Animes :', true), null, ok));
     let MainPageObject = new MainPage(new ModuleRequest('https://www.ianimes.org/index.php', 'get', emptyKeyValue,null),
     new Extra([new Commands('', emptyKeyValue)], emptyKeyValue),
     new JavascriptConfig(true,false, ''), output);
